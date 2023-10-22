5658a24494caf6e18a3d1e97978147dd18825135
Tobias Kastl <if22b007@technikum-wien.at>
2023-10-22 22:44:47 +0200

    DbHandler
    First steps with docker, postgres & npgsql

---
+++ repos/MTCG/DbHandler.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Npgsql;
+
+namespace MTCG
+{
+    public class DbHandler
+    {
+        private readonly string _connectionString;
+
+        public DbHandler(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public NpgsqlConnection GetConnection()
+        {
+            return new NpgsqlConnection(_connectionString);
+        }
+
+        public void OpenConnection(NpgsqlConnection connection)
+        {
+            if (connection.State != ConnectionState.Open)
+            {
+                connection.Open();
+            }
+        }
+
+        public void CloseConnection(NpgsqlConnection connection)
+        {
+            if (connection.State == ConnectionState.Open)
+            {
+                connection.Close();
+            }
+        }
+
+        public NpgsqlDataReader ExecuteQuery(string query)
+        {
+            using (var connection = GetConnection())
+            {
+                OpenConnection(connection);
+                using (var command = new NpgsqlCommand(query, connection))
+                {
+                    return command.ExecuteReader();
+                }
+            }
+        }
+    }
+}
