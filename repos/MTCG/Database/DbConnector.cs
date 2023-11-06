7d88ac7eed1810ac6362b5f3e2c95f5468dc4c57
Tobias Kastl <if22b007@technikum-wien.at>
2023-11-06 17:19:21 +0100

    Created Database in Docker-Container "MTCG" & created two tables

---
+++ repos/MTCG/Database/DbConnector.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Npgsql;
+using System.Data.SqlClient;
+
+namespace MTCG.Database
+{
+    public class DbConnector
+    {
+        private const string HOST = "localhost";
+        private const int PORT = 5432;
+        private const string DBNAME = "MTCG_DB";
+        private const string USERNAME = "postgres";
+        private const string PASSWORD = "MTCG";
+
+        public NpgsqlConnection CreateConnection()
+        {
+            var builder = new NpgsqlConnectionStringBuilder
+            {
+                Host = HOST,
+                Port = PORT,
+                Database = DBNAME,
+                Username = USERNAME,
+                Password = PASSWORD
+            };
+
+            NpgsqlConnection connection = new NpgsqlConnection(builder.ToString());
+
+            return connection;
+        }
+    }
+}
--- repos/MTCG/Program.cs
+++ repos/MTCG/Program.cs
@@ -1,9 +1,12 @@
-﻿namespace MTCG
+﻿using MTCG.Database;
+
+namespace MTCG
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            /*
             Shop shop = new Shop();
 
             Champion c1 = shop.SelectRandomChampion();
@@ -11,6 +14,15 @@
 
             Console.WriteLine(c1 + "\n");
             Console.WriteLine(s1);
+            */
+
+            var dbConnector = new DbConnector();
+            using (var connection = dbConnector.CreateConnection())
+            {
+                connection.Open();
+                // Die Verbindung ist jetzt geöffnet und Sie können Datenbankoperationen durchführen.
+            }
+
         }
     }
 }
